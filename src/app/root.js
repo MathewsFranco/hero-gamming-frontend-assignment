0b1dd7a633b303f9072dbd3f7935abbb52749bdc
mathewsfranco <mathewsandre.franco@gmail.com>
2022-08-18 00:45:55 +0200

    🛠remove unused code

--- src/app/root.js
+++ src/app/root.js
@@ -3,13 +3,7 @@ import './main.css';
 import * as React from 'react';
 
 import { UniversalRouter } from './universal-router';
-import { useLocation } from 'react-router-dom';
 
 export function Root(props) {
-  // const createNewStopWatch = async () => {
-  //   const started = newDate();
-  //   console.log(`🚀 ~ started`, started);
-  // };
-
   return <UniversalRouter location={props.location} />;
 }
